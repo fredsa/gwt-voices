0a8dfbb18a34e39e40e75ff87abed54359edebc6
Fred Sauer <fredsa@gmail.com>
2007-09-28 16:40:03 +0000

    add test launch code
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk@31 7bc97cf3-a739-0410-90d3-b551d870f104

---
+++ Voices/test/com/allen_sauer/gwt/voices/test/client/VoicesTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2007 Fred Sauer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.allen_sauer.gwt.voices.test.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
+
+public class VoicesTest implements EntryPoint {
+  private static final String DEMO_EVENT_TEXT_AREA = "demo-event-text-area";
+  private static final String DEMO_SOUNDS_PANEL = "demo-sounds-panel";
+
+  public void onModuleLoad() {
+    // set uncaught exception handler
+    // GWT.setUncaughtExceptionHandler(new LogUncaughtExceptionHandler() {
+    // });
+
+    // use deferred command to catch initialization exceptions
+    DeferredCommand.addCommand(new Command() {
+      public void execute() {
+        onModuleLoad2();
+      }
+    });
+  }
+
+  public void onModuleLoad2() {
+  }
+}
