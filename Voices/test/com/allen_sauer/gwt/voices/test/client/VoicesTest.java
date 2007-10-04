418e11b00757b97738086baa893fa3e02693d42e
Fred Sauer <fredsa@gmail.com>
2007-10-04 20:11:35 +0000

    small test launch
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk@37 7bc97cf3-a739-0410-90d3-b551d870f104

--- Voices/test/com/allen_sauer/gwt/voices/test/client/VoicesTest.java
+++ Voices/test/com/allen_sauer/gwt/voices/test/client/VoicesTest.java
@@ -16,17 +16,29 @@
 package com.allen_sauer.gwt.voices.test.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import com.allen_sauer.gwt.log.client.LogUncaughtExceptionHandler;
+import com.allen_sauer.gwt.voices.client.Sound;
+import com.allen_sauer.gwt.voices.client.SoundController;
 
 public class VoicesTest implements EntryPoint {
-  private static final String DEMO_EVENT_TEXT_AREA = "demo-event-text-area";
-  private static final String DEMO_SOUNDS_PANEL = "demo-sounds-panel";
+  private static native String getCompatMode()
+  /*-{
+    return $doc.compatMode;
+  }-*/;
 
   public void onModuleLoad() {
     // set uncaught exception handler
-    // GWT.setUncaughtExceptionHandler(new LogUncaughtExceptionHandler() {
-    // });
+    GWT.setUncaughtExceptionHandler(new LogUncaughtExceptionHandler() {
+    });
 
     // use deferred command to catch initialization exceptions
     DeferredCommand.addCommand(new Command() {
@@ -37,5 +49,17 @@ public class VoicesTest implements EntryPoint {
   }
 
   public void onModuleLoad2() {
+    RootPanel.get().add(
+        new HTML("VoicesTest is in <b>" + getCompatMode() + "</b> mode."));
+    SoundController soundController = new SoundController();
+    final Sound sound = soundController.createSound(Sound.MIME_TYPE_AUDIO_MPEG,
+        "freesoundproject/33637__HerbertBoland__CinematicBoomNorm.mp3");
+    Button button = new Button("play");
+    button.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        sound.play();
+      }
+    });
+    RootPanel.get().add(button);
   }
 }
