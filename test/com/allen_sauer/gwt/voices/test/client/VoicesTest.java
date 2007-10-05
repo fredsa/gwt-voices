9cc49f5dd5bacb543f6f9f9ab461d5e6b716174b
Fred Sauer <fredsa@gmail.com>
2007-10-05 16:23:05 +0000

    light weight replacements for gwt-log
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk/Voices@43 7bc97cf3-a739-0410-90d3-b551d870f104

--- test/com/allen_sauer/gwt/voices/test/client/VoicesTest.java
+++ test/com/allen_sauer/gwt/voices/test/client/VoicesTest.java
@@ -18,14 +18,15 @@ package com.allen_sauer.gwt.voices.test.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-import com.allen_sauer.gwt.log.client.LogUncaughtExceptionHandler;
 import com.allen_sauer.gwt.voices.client.Sound;
 import com.allen_sauer.gwt.voices.client.SoundController;
 
@@ -37,7 +38,27 @@ public class VoicesTest implements EntryPoint {
 
   public void onModuleLoad() {
     // set uncaught exception handler
-    GWT.setUncaughtExceptionHandler(new LogUncaughtExceptionHandler() {
+    GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler() {
+      public void onUncaughtException(Throwable throwable) {
+        String text = "Uncaught exception: ";
+        while (throwable != null) {
+          StackTraceElement[] stackTraceElements = throwable.getStackTrace();
+          text += new String(throwable.toString() + "\n");
+          for (int i = 0; i < stackTraceElements.length; i++) {
+            text += "    at " + stackTraceElements[i] + "\n";
+          }
+          throwable = throwable.getCause();
+          if (throwable != null) {
+            text += "Caused by: ";
+          }
+        }
+        DialogBox dialogBox = new DialogBox(true);
+        DOM.setStyleAttribute(dialogBox.getElement(), "backgroundColor",
+            "#ABCDEF");
+        text = text.replaceAll(" ", "&nbsp;");
+        dialogBox.setHTML("<pre>" + text + "</pre>");
+        dialogBox.center();
+      }
     });
 
     // use deferred command to catch initialization exceptions
