d73d2e3a7c31555fae8d53276f451f8cd40269ac
Fred Sauer <fredsa@gmail.com>
2007-12-11 17:49:33 +0000

    updated animated GIF and CSS for 'loading...' message
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk/Voices@66 7bc97cf3-a739-0410-90d3-b551d870f104

--- demo/com/allen_sauer/gwt/voices/demo/client/ui/DeferredContentPanel.java
+++ demo/com/allen_sauer/gwt/voices/demo/client/ui/DeferredContentPanel.java
@@ -24,8 +24,8 @@ public abstract class DeferredContentPanel extends Composite {
   public DeferredContentPanel() {
     SimplePanel simplePanel = new SimplePanel();
     initWidget(simplePanel);
-    simplePanel.setWidget(new HTML("<img src='images/preview61_cycle_eight_16-black-on-white.gif' class='demo-loading'>"
-        + "Loading. Please wait..."));
+    simplePanel.setWidget(new HTML("<div class='demo-loading'><img src='images/preview61_cycle_eight_16-black-on-white.gif'>"
+        + "Loading. Please wait...</div>"));
   }
 
   public abstract Panel initContent();
