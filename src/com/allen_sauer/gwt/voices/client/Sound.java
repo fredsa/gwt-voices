f1743c2844d2a8f7e69fe844a4ec8fac419f0a42
Fred Sauer <fredsa@gmail.com>
2008-03-30 05:13:16 +0000

    checkstyle cleanup
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk/Voices@96 7bc97cf3-a739-0410-90d3-b551d870f104

--- src/com/allen_sauer/gwt/voices/client/Sound.java
+++ src/com/allen_sauer/gwt/voices/client/Sound.java
@@ -18,63 +18,63 @@ package com.allen_sauer.gwt.voices.client;
 import com.allen_sauer.gwt.voices.client.handler.FiresSoundEvents;
 
 public interface Sound extends FiresSoundEvents {
-  public static final int LOAD_STATE_LOADED = 6;
-  public static final int LOAD_STATE_SUPPORTED = 4;
-  public static final int LOAD_STATE_SUPPORTED_NOT_LOADED = 5;
-  public static final int LOAD_STATE_UNINITIALIZED = 1;
-  public static final int LOAD_STATE_UNKNOWN = 2;
-  public static final int LOAD_STATE_UNSUPPORTED = 3;
+  int LOAD_STATE_LOADED = 6;
+  int LOAD_STATE_SUPPORTED = 4;
+  int LOAD_STATE_SUPPORTED_NOT_LOADED = 5;
+  int LOAD_STATE_UNINITIALIZED = 1;
+  int LOAD_STATE_UNKNOWN = 2;
+  int LOAD_STATE_UNSUPPORTED = 3;
 
   /**
    * IANA assigned media type <code>audio/basic</code> for RFC 2045/2046.
    * Typical filename extensions include <code>.au</code> and
    * <code>.snd</code>.
    */
-  public static final String MIME_TYPE_AUDIO_BASIC = "audio/basic";
+  String MIME_TYPE_AUDIO_BASIC = "audio/basic";
 
   /**
    * IANA assigned media type <code>audio/mpeg</code> for RFC 3003. Typical
    * filename extensions include <code>.mp1</code>, <code>.mp2</code> and
    * <code>.mp3</code>.
    */
-  public static final String MIME_TYPE_AUDIO_MPEG = "audio/mpeg";
+  String MIME_TYPE_AUDIO_MPEG = "audio/mpeg";
 
   /**
    * Using <code>audio/x-aiff</code> instead of the more popular, but
    * unregistered, <code>audio/aiff</code>. Typical filename extension is
    * <code>.aif</code>.
    */
-  public static final String MIME_TYPE_AUDIO_X_AIFF = "audio/x-aiff";
+  String MIME_TYPE_AUDIO_X_AIFF = "audio/x-aiff";
 
   /**
    * Using <code>audio/x-midi</code> instead of the more popular, but
    * unregistered, <code>audio/midi</code>. Typical filename extensions
    * include <code>.mid</code> and <code>.midi</code>.
    */
-  public static final String MIME_TYPE_AUDIO_X_MIDI = "audio/x-midi";
+  String MIME_TYPE_AUDIO_X_MIDI = "audio/x-midi";
 
   /**
    * Using <code>audio/x-wav</code> instead of the more popular, but
    * unregistered, <code>audio/wav</code>. Typical filename extension is
    * <code>.wav</code>.
    */
-  public static final String MIME_TYPE_AUDIO_X_WAV = "audio/x-wav";
+  String MIME_TYPE_AUDIO_X_WAV = "audio/x-wav";
 
-  public int getLoadState();
+  int getLoadState();
 
-  public String getMimeType();
+  String getMimeType();
 
-  public String getSoundType();
+  String getSoundType();
 
-  public String getUrl();
+  String getUrl();
 
-  public int getVolume();
+  int getVolume();
 
-  public void play();
+  void play();
 
-  public void setBalance(int balance);
+  void setBalance(int balance);
 
-  public void setVolume(int volume);
+  void setVolume(int volume);
 
-  public void stop();
+  void stop();
 }
