515ee6385e313c5c57ce91e9c341faf8aba75bb7
Fred Sauer <fredsa@gmail.com>
2008-06-23 04:53:32 +0000

    for prosterity
    git-svn-id: https://gwt-voices.googlecode.com/svn/tags/gwt-voices-1.5.4-r160@161 7bc97cf3-a739-0410-90d3-b551d870f104

--- src/com/allen_sauer/gwt/voices/client/NativeSound.java
+++ src/com/allen_sauer/gwt/voices/client/NativeSound.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 Fred Sauer
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -36,8 +36,8 @@ public class NativeSound extends AbstractSound {
   private final NativeSoundWidget nativeSoundWidget;
   private int volume;
 
-  public NativeSound(String mimeType, String url, Element soundControllerElement) {
-    super(mimeType, url);
+  public NativeSound(String mimeType, String url, boolean streaming, Element soundControllerElement) {
+    super(mimeType, url, streaming);
     nativeSoundWidget = new NativeSoundWidget(soundControllerElement, mimeType, url);
     MimeTypeSupport mimeTypeSupport = NativeSoundWidget.getMimeTypeSupport(mimeType);
     switch (mimeTypeSupport) {
