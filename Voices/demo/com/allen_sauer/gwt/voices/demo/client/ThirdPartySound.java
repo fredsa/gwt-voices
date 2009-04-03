693fc8d28b902594f46e1d3dfd00d16907acd56b
Fred Sauer <fredsa@gmail.com>
2009-04-03 22:46:20 +0000

    formatting
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk@178 7bc97cf3-a739-0410-90d3-b551d870f104

--- Voices/demo/com/allen_sauer/gwt/voices/demo/client/ThirdPartySound.java
+++ Voices/demo/com/allen_sauer/gwt/voices/demo/client/ThirdPartySound.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2008 Fred Sauer
- *
+ * Copyright 2009 Fred Sauer
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -19,7 +19,7 @@ import com.allen_sauer.gwt.voices.client.Sound;
 
 // CHECKSTYLE_JAVADOC_OFF
 public abstract class ThirdPartySound {
-  private String mimeType;
+  private final String mimeType;
   private Sound sound;
 
   public ThirdPartySound(String mimeType) {
--- Voices/src/com/allen_sauer/gwt/voices/client/ui/impl/NativeSoundImplIE6.java
+++ Voices/src/com/allen_sauer/gwt/voices/client/ui/impl/NativeSoundImplIE6.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2008 Fred Sauer
- *
+ * Copyright 2009 Fred Sauer
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,14 +15,14 @@
  */
 package com.allen_sauer.gwt.voices.client.ui.impl;
 
+import static com.allen_sauer.gwt.voices.client.SoundController.MimeTypeSupport.MIME_TYPE_NOT_SUPPORTED;
+import static com.allen_sauer.gwt.voices.client.SoundController.MimeTypeSupport.MIME_TYPE_SUPPORT_READY;
+
 import com.google.gwt.user.client.Element;
 
 import com.allen_sauer.gwt.voices.client.Sound;
 import com.allen_sauer.gwt.voices.client.SoundController.MimeTypeSupport;
 import com.allen_sauer.gwt.voices.client.util.StringUtil;
-
-import static com.allen_sauer.gwt.voices.client.SoundController.MimeTypeSupport.MIME_TYPE_NOT_SUPPORTED;
-import static com.allen_sauer.gwt.voices.client.SoundController.MimeTypeSupport.MIME_TYPE_SUPPORT_READY;
 
 import java.util.HashMap;
 
