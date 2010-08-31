6ad9dbda8fd81a667393b2d3e9e737e48d59cf86
Fred Sauer <fredsa@google.com>
2010-08-31 21:24:55 +0000

    Cleanup demo mime types
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk@312 7bc97cf3-a739-0410-90d3-b551d870f104

--- Voices/demo/com/allen_sauer/gwt/voices/demo/client/VoicesDemo.java
+++ Voices/demo/com/allen_sauer/gwt/voices/demo/client/VoicesDemo.java
@@ -34,105 +34,35 @@ import com.allen_sauer.gwt.voices.demo.client.ui.MimeTypeDemo;
 import com.allen_sauer.gwt.voices.demo.client.ui.SupportedMimeTypeSummary;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 // CHECKSTYLE_JAVADOC_OFF
 public class VoicesDemo implements EntryPoint {
-  private static ThirdPartySound[] freeSounds;
+  private static ThirdPartySound[] thirdPartySounds;
   private static HashMap<String, ArrayList<ThirdPartySound>> mimeTypeSoundMap = new HashMap<
       String, ArrayList<ThirdPartySound>>();
 
-  /**
-   * Using <code>audio/wav</code> instead of the more popular, but unregistered,
-   * <code>audio/wav</code>. Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_WAV = "audio/wav";
-
-  /**
-   * Using <code>audio/x-wav</code> instead of the more popular, but unregistered,
-   * <code>audio/wav</code>. Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_VND_WAV = "audio/vnd.wave";
-
-  /**
-   * MIME Type <code>audio/vnd.wave; codecs=0</code> for WAVE audio in unknown format.
-   * See RFC 2361 (WAVE and AVI Codec Registries).
-   * Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_VND_WAVE_UNKNOWN = "audio/vnd.wave; codecs=0";
-
-  /**
-   * MIME Type <code>audio/x-wav; codecs=0</code> for WAVE audio in unknown format.
-   * See RFC 2361 (WAVE and AVI Codec Registries).
-   * Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_X_WAV_UNKNOWN = "audio/x-wav; codecs=0";
-
-  /**
-   * MIME Type <code>audio/vnd.wave; codecs=1</code> for WAVE audio in Microsoft PCM format.
-   * See RFC 2361 (WAVE and AVI Codec Registries).
-   * Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_VND_WAVE_PCM = "audio/vnd.wave; codecs=1";
-
-  /**
-   * MIME Type <code>audio/x-wav; codecs=1</code> for WAVE audio in Microsoft PCM format.
-   * See RFC 2361 (WAVE and AVI Codec Registries).
-   * Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_X_WAV_PCM = "audio/x-wav; codecs=1";
-
-  /**
-   * MIME Type <code>audio/vnd.wave; codecs=2</code> for WAVE audio in Microsoft ADPCM format.
-   * See RFC 2361 (WAVE and AVI Codec Registries).
-   * Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_VND_WAVE_ADPCM = "audio/vnd-wave; codecs=2";
-
-  /**
-   * MIME Type <code>audio/x-wav; codecs=2</code> for WAVE audio in Microsoft ADPCM format.
-   * See RFC 2361 (WAVE and AVI Codec Registries).
-   * Typical filename extension is <code>.wav</code>.
-   */
-  @Deprecated
-  private static String MIME_TYPE_AUDIO_X_WAV_ADPCM = "audio/x-wav; codecs=2";
-
-  @SuppressWarnings("deprecation")
-  public static String[] MIME_TYPES = {
-      Sound.MIME_TYPE_AUDIO_BASIC, Sound.MIME_TYPE_AUDIO_MPEG, Sound.MIME_TYPE_AUDIO_MP4,
-      Sound.MIME_TYPE_AUDIO_MP4_MP4A_40_2, Sound.MIME_TYPE_AUDIO_X_AIFF,
-      Sound.MIME_TYPE_AUDIO_X_MIDI, MIME_TYPE_AUDIO_WAV, Sound.MIME_TYPE_AUDIO_X_WAV,
-      MIME_TYPE_AUDIO_VND_WAV, Sound.MIME_TYPE_AUDIO_WAV_UNKNOWN, MIME_TYPE_AUDIO_X_WAV_UNKNOWN,
-      MIME_TYPE_AUDIO_VND_WAVE_UNKNOWN, Sound.MIME_TYPE_AUDIO_WAV_PCM, MIME_TYPE_AUDIO_X_WAV_PCM,
-      MIME_TYPE_AUDIO_VND_WAVE_PCM, Sound.MIME_TYPE_AUDIO_WAV_ADPCM, MIME_TYPE_AUDIO_X_WAV_ADPCM,
-      MIME_TYPE_AUDIO_VND_WAVE_ADPCM, Sound.MIME_TYPE_AUDIO_OGG, Sound.MIME_TYPE_AUDIO_OGG_FLAC,
-      Sound.MIME_TYPE_AUDIO_OGG_SPEEX, Sound.MIME_TYPE_AUDIO_OGG_VORBIS,};
-
   static {
-    freeSounds = new ThirdPartySound[] {
-        new FreesoundProjectSound(MIME_TYPE_AUDIO_VND_WAVE_PCM,
+    thirdPartySounds = new ThirdPartySound[] {
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_WAV_PCM,
             "freesoundproject/35631__reinsamba__crystal_glass.wav", "crystal_glass",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=35631", "reinsamba",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=18799",
             "RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, stereo 44100 Hz"),
-        new FreesoundProjectSound(MIME_TYPE_AUDIO_VND_WAVE_PCM,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_WAV_PCM,
             "freesoundproject/38403__THE_bizniss__snap.wav", "snap",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=38403", "THE_bizniss",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=382028",
             "RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 44100 Hz"),
-        new FreesoundProjectSound(MIME_TYPE_AUDIO_VND_WAVE_PCM,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_WAV_PCM,
             "freesoundproject/22740__FranciscoPadilla__37_Click_Finger.wav", "37 Click Finger",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=22740", "FranciscoPadilla",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=132693",
             "RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 44100 Hz"),
-        new FreesoundProjectSound(MIME_TYPE_AUDIO_VND_WAVE_ADPCM,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_WAV_ADPCM,
             "freesoundproject/9874__vixuxx__crow.wav", "crow",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=9874", "vixuxx",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=28679",
@@ -146,37 +76,37 @@ public class VoicesDemo implements EntryPoint {
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=9874", "vixuxx",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=28679",
             "Sun/NeXT audio data: 16-bit linear PCM, mono, 22050 Hz"),
-        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/28917__junggle__btn107.mp3", "btn107",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=28917", "junggle",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=128404",
             "Audio file with ID3 version 23.0 tag, MP3 encoding"),
-        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/36846__EcoDTR__LaserRocket.mp3", "LaserRocket",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=36846", "EcoDTR",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=181367",
             "Audio file with ID3 version 23.0 tag, MP3 encoding"),
-        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/35643__sandyrb__USAT_BOMB.mp3", "USAT BOMB",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=35643", "sandyrb",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=14771",
             "Audio file with ID3 version 23.0 tag, MP3 encoding"),
-        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/34961__grandpablaine2__grenade_reverse_reverb.mp3",
             "grenade_reverse_reverb", "http://freesound.iua.upf.edu/samplesViewSingle.php?id=34961",
             "grandpablaine2", "http://freesound.iua.upf.edu/usersViewSingle.php?id=147084",
             "Audio file with ID3 version 2\\012- 2.\\012- 0 tag\\012- , MP3 encoding"),
-        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/33637__HerbertBoland__CinematicBoomNorm.mp3", "CinematicBoomNorm",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=33637", "HerbertBoland",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=129090",
             "Audio file with ID3 version 2\\012- 2.\\012- 0 tag\\012- , MP3 encoding"),
-        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_OGG,
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_OGG_VORBIS,
             "freesoundproject/91960__billengholm_yahoo.com__opensurdo.ogg", "opensurdo",
             "http://www.freesound.org/samplesViewSingle.php?id=91960", "billengholm@yahoo.com",
             "http://www.freesound.org/usersViewSingle.php?id=1513331",
             "Ogg data, Vorbis audio, stereo, 44100 Hz, ~128000 bps"),
-        new WikipediaSound(Sound.MIME_TYPE_AUDIO_OGG, "wikipedia/En-us-squid.ogg",
+        new WikipediaSound(Sound.MIME_TYPE_AUDIO_OGG_VORBIS, "wikipedia/En-us-squid.ogg",
             "En-us-squid.ogg", "http://upload.wikimedia.org/wikipedia/commons/f/f1/En-us-squid.ogg",
             "http://en.wiktionary.org/wiki/squid",
             "Ogg data, Vorbis audio, mono, 44100 Hz, ~96001 bps"),
@@ -189,18 +119,13 @@ public class VoicesDemo implements EntryPoint {
             "Drum_sample.mid", "http://upload.wikimedia.org/wikipedia/commons/6/61/Drum_sample.mid",
             "http://en.wikipedia.org/wiki/Musical_Instrument_Digital_Interface",
             "Standard MIDI data (format 1) using 2 tracks at 1/240"),};
-
-    for (ThirdPartySound element : freeSounds) {
-      String mimeType = element.getMimeType();
-      ArrayList<ThirdPartySound> freesoundList = mimeTypeSoundMap.get(mimeType);
-      if (freesoundList == null) {
-        freesoundList = new ArrayList<ThirdPartySound>();
-        mimeTypeSoundMap.put(mimeType, freesoundList);
-      }
-      freesoundList.add(element);
-    }
   }
 
+  public static String[] MIME_TYPES = {
+      Sound.MIME_TYPE_AUDIO_OGG_VORBIS, Sound.MIME_TYPE_AUDIO_MPEG_MP3,
+      Sound.MIME_TYPE_AUDIO_WAV_PCM, Sound.MIME_TYPE_AUDIO_WAV_ADPCM, Sound.MIME_TYPE_AUDIO_BASIC,
+      Sound.MIME_TYPE_AUDIO_X_AIFF, Sound.MIME_TYPE_AUDIO_X_MIDI,};
+  
   public void onModuleLoad() {
     // set uncaught exception handler
     GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler() {
@@ -217,7 +142,7 @@ public class VoicesDemo implements EntryPoint {
             text += "Caused by: ";
           }
         }
-        DialogBox dialogBox = new DialogBox(true);
+        DialogBox dialogBox = new DialogBox(true, false);
         DOM.setStyleAttribute(dialogBox.getElement(), "backgroundColor", "#ABCDEF");
         System.err.print(text);
         text = text.replaceAll(" ", "&nbsp;");
@@ -257,6 +182,20 @@ public class VoicesDemo implements EntryPoint {
     crowdSourceSupportMatrix.setContent(crowdSourceFrame);
     mainPanel.add(crowdSourceSupportMatrix);
 
+    // initialize mimeTypeSoundMap
+    Set<String> mimeTypeSet = new HashSet<String>(Arrays.asList(MIME_TYPES));
+    for (ThirdPartySound thirdPartySound : thirdPartySounds) {
+      String mimeType = thirdPartySound.getMimeType();
+      assert (mimeTypeSet.contains(mimeType)) : "MIME_TYPES must contain '" + mimeType + "'";
+      ArrayList<ThirdPartySound> freesoundList = mimeTypeSoundMap.get(mimeType);
+      if (freesoundList == null) {
+        freesoundList = new ArrayList<ThirdPartySound>();
+        mimeTypeSoundMap.put(mimeType, freesoundList);
+      }
+      freesoundList.add(thirdPartySound);
+    }
+
+    // display one panel for each unique mime type, using the order supplied by MIME_TYPES
     for (String mimeType : MIME_TYPES) {
       ArrayList<ThirdPartySound> soundList = mimeTypeSoundMap.get(mimeType);
       if (soundList != null) {
