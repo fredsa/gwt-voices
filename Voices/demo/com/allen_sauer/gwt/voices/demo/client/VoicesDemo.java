d1bcbacf29eb04e7f52556c31f61984324990f17
Fred Sauer <fredsa@google.com>
2010-10-23 18:46:31 +0000

    Include both audio/mpeg; codecs=MP3 (for HTML5 capable browsers) and audio/mpeg (for Firefox 3.6)
    Suggested by: jamestyner
    
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk@323 7bc97cf3-a739-0410-90d3-b551d870f104

--- Voices/demo/com/allen_sauer/gwt/voices/demo/client/VoicesDemo.java
+++ Voices/demo/com/allen_sauer/gwt/voices/demo/client/VoicesDemo.java
@@ -1,27 +1,22 @@
 /*
  * Copyright 2010 Fred Sauer
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.allen_sauer.gwt.voices.demo.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.BorderStyle;
 import com.google.gwt.dom.client.StyleInjector;
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.DataResource;
-import com.google.gwt.resources.client.DataResource.MimeType;
+import com.google.gwt.dom.client.Style.BorderStyle;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.DeferredCommand;
@@ -44,9 +39,17 @@ import java.util.TreeSet;
 
 // CHECKSTYLE_JAVADOC_OFF
 public class VoicesDemo implements EntryPoint {
+  @SuppressWarnings("deprecation")
+  public static SortedSet<String> MIME_TYPES = new TreeSet<String>(Arrays.asList(new String[] {
+      Sound.MIME_TYPE_AUDIO_BASIC, Sound.MIME_TYPE_AUDIO_MPEG_MP3, Sound.MIME_TYPE_AUDIO_MPEG,
+      Sound.MIME_TYPE_AUDIO_MP4, Sound.MIME_TYPE_AUDIO_MP4_MP4A_40_2, Sound.MIME_TYPE_AUDIO_X_AIFF,
+      Sound.MIME_TYPE_AUDIO_X_MIDI, Sound.MIME_TYPE_AUDIO_WAV_UNKNOWN,
+      Sound.MIME_TYPE_AUDIO_WAV_PCM, Sound.MIME_TYPE_AUDIO_WAV_ADPCM, Sound.MIME_TYPE_AUDIO_OGG,
+      Sound.MIME_TYPE_AUDIO_OGG_FLAC, Sound.MIME_TYPE_AUDIO_OGG_SPEEX,
+      Sound.MIME_TYPE_AUDIO_OGG_VORBIS,}));
+  private static HashMap<String, ArrayList<ThirdPartySound>> mimeTypeSoundMap = new HashMap<String, ArrayList<ThirdPartySound>>();
+
   private static ThirdPartySound[] thirdPartySounds;
-  private static HashMap<String, ArrayList<ThirdPartySound>> mimeTypeSoundMap = new HashMap<
-      String, ArrayList<ThirdPartySound>>();
 
   static {
     thirdPartySounds = new ThirdPartySound[] {
@@ -79,6 +82,37 @@ public class VoicesDemo implements EntryPoint {
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=9874", "vixuxx",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=28679",
             "Sun/NeXT audio data: 16-bit linear PCM, mono, 22050 Hz"),
+
+        // audio/mpeg
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+            "freesoundproject/28917__junggle__btn107.mp3", "btn107",
+            "http://freesound.iua.upf.edu/samplesViewSingle.php?id=28917", "junggle",
+            "http://freesound.iua.upf.edu/usersViewSingle.php?id=128404",
+            "Audio file with ID3 version 23.0 tag, MP3 encoding"),
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+            "freesoundproject/36846__EcoDTR__LaserRocket.mp3", "LaserRocket",
+            "http://freesound.iua.upf.edu/samplesViewSingle.php?id=36846", "EcoDTR",
+            "http://freesound.iua.upf.edu/usersViewSingle.php?id=181367",
+            "Audio file with ID3 version 23.0 tag, MP3 encoding"),
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+            "freesoundproject/35643__sandyrb__USAT_BOMB.mp3", "USAT BOMB",
+            "http://freesound.iua.upf.edu/samplesViewSingle.php?id=35643", "sandyrb",
+            "http://freesound.iua.upf.edu/usersViewSingle.php?id=14771",
+            "Audio file with ID3 version 23.0 tag, MP3 encoding"),
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+            "freesoundproject/34961__grandpablaine2__grenade_reverse_reverb.mp3",
+            "grenade_reverse_reverb",
+            "http://freesound.iua.upf.edu/samplesViewSingle.php?id=34961", "grandpablaine2",
+            "http://freesound.iua.upf.edu/usersViewSingle.php?id=147084",
+            "Audio file with ID3 version 2\\012- 2.\\012- 0 tag\\012- , MP3 encoding"),
+        new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG,
+            "freesoundproject/33637__HerbertBoland__CinematicBoomNorm.mp3", "CinematicBoomNorm",
+            "http://freesound.iua.upf.edu/samplesViewSingle.php?id=33637", "HerbertBoland",
+            "http://freesound.iua.upf.edu/usersViewSingle.php?id=129090",
+            "Audio file with ID3 version 2\\012- 2.\\012- 0 tag\\012- , MP3 encoding"),
+
+        // audio/mpeg; codecs=MP3
+
         new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/28917__junggle__btn107.mp3", "btn107",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=28917", "junggle",
@@ -96,21 +130,24 @@ public class VoicesDemo implements EntryPoint {
             "Audio file with ID3 version 23.0 tag, MP3 encoding"),
         new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/34961__grandpablaine2__grenade_reverse_reverb.mp3",
-            "grenade_reverse_reverb", "http://freesound.iua.upf.edu/samplesViewSingle.php?id=34961",
-            "grandpablaine2", "http://freesound.iua.upf.edu/usersViewSingle.php?id=147084",
+            "grenade_reverse_reverb",
+            "http://freesound.iua.upf.edu/samplesViewSingle.php?id=34961", "grandpablaine2",
+            "http://freesound.iua.upf.edu/usersViewSingle.php?id=147084",
             "Audio file with ID3 version 2\\012- 2.\\012- 0 tag\\012- , MP3 encoding"),
         new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,
             "freesoundproject/33637__HerbertBoland__CinematicBoomNorm.mp3", "CinematicBoomNorm",
             "http://freesound.iua.upf.edu/samplesViewSingle.php?id=33637", "HerbertBoland",
             "http://freesound.iua.upf.edu/usersViewSingle.php?id=129090",
             "Audio file with ID3 version 2\\012- 2.\\012- 0 tag\\012- , MP3 encoding"),
+
         new FreesoundProjectSound(Sound.MIME_TYPE_AUDIO_OGG_VORBIS,
             "freesoundproject/91960__billengholm_yahoo.com__opensurdo.ogg", "opensurdo",
             "http://www.freesound.org/samplesViewSingle.php?id=91960", "billengholm@yahoo.com",
             "http://www.freesound.org/usersViewSingle.php?id=1513331",
             "Ogg data, Vorbis audio, stereo, 44100 Hz, ~128000 bps"),
         new WikipediaSound(Sound.MIME_TYPE_AUDIO_OGG_VORBIS, "wikipedia/En-us-squid.ogg",
-            "En-us-squid.ogg", "http://upload.wikimedia.org/wikipedia/commons/f/f1/En-us-squid.ogg",
+            "En-us-squid.ogg",
+            "http://upload.wikimedia.org/wikipedia/commons/f/f1/En-us-squid.ogg",
             "http://en.wiktionary.org/wiki/squid",
             "Ogg data, Vorbis audio, mono, 44100 Hz, ~96001 bps"),
         new WikipediaSound(Sound.MIME_TYPE_AUDIO_X_MIDI, "wikipedia/Bass_sample2.mid",
@@ -119,22 +156,11 @@ public class VoicesDemo implements EntryPoint {
             "http://en.wikipedia.org/wiki/Musical_Instrument_Digital_Interface",
             "Standard MIDI data (format 1) using 2 tracks at 1/240"),
         new WikipediaSound(Sound.MIME_TYPE_AUDIO_X_MIDI, "wikipedia/Drum_sample.mid",
-            "Drum_sample.mid", "http://upload.wikimedia.org/wikipedia/commons/6/61/Drum_sample.mid",
+            "Drum_sample.mid",
+            "http://upload.wikimedia.org/wikipedia/commons/6/61/Drum_sample.mid",
             "http://en.wikipedia.org/wiki/Musical_Instrument_Digital_Interface",
             "Standard MIDI data (format 1) using 2 tracks at 1/240"),};
   }
-
-  @SuppressWarnings("deprecation")
-  public static SortedSet<String> MIME_TYPES = new TreeSet<String>(
-      Arrays.asList(
-          new String[] {
-              Sound.MIME_TYPE_AUDIO_BASIC, Sound.MIME_TYPE_AUDIO_MPEG_MP3,
-              Sound.MIME_TYPE_AUDIO_MPEG, Sound.MIME_TYPE_AUDIO_MP4,
-              Sound.MIME_TYPE_AUDIO_MP4_MP4A_40_2, Sound.MIME_TYPE_AUDIO_X_AIFF,
-              Sound.MIME_TYPE_AUDIO_X_MIDI, Sound.MIME_TYPE_AUDIO_WAV_UNKNOWN,
-              Sound.MIME_TYPE_AUDIO_WAV_PCM, Sound.MIME_TYPE_AUDIO_WAV_ADPCM,
-              Sound.MIME_TYPE_AUDIO_OGG, Sound.MIME_TYPE_AUDIO_OGG_FLAC,
-              Sound.MIME_TYPE_AUDIO_OGG_SPEEX, Sound.MIME_TYPE_AUDIO_OGG_VORBIS,}));
 
   public void onModuleLoad() {
     // set uncaught exception handler
@@ -195,7 +221,7 @@ public class VoicesDemo implements EntryPoint {
     // initialize mimeTypeSoundMap
     for (ThirdPartySound thirdPartySound : thirdPartySounds) {
       String mimeType = thirdPartySound.getMimeType();
-      assert (MIME_TYPES.contains(mimeType)) : "MIME_TYPES must contain '" + mimeType + "'";
+      assert MIME_TYPES.contains(mimeType) : "MIME_TYPES must contain '" + mimeType + "'";
       ArrayList<ThirdPartySound> freesoundList = mimeTypeSoundMap.get(mimeType);
       if (freesoundList == null) {
         freesoundList = new ArrayList<ThirdPartySound>();
