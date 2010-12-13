63901f5bb484435b2fa92831875e332b348153b4
Fred Sauer <fredsa@google.com>
2010-12-13 18:21:20 +0000

    Add {@value} to javadoc
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk@325 7bc97cf3-a739-0410-90d3-b551d870f104

--- Voices/src/com/allen_sauer/gwt/voices/client/ui/FlashMovie.java
+++ Voices/src/com/allen_sauer/gwt/voices/client/ui/FlashMovie.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Fred Sauer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -27,9 +27,13 @@ public class FlashMovie {
   // CHECKSTYLE_JAVADOC_OFF
 
   /**
-   * Flash movie MIME type. Typical filename extension is <code>.swf</code>.
+   * Flash movie MIME type (<code>{@value}</code>). Typical filename extension is <code>.swf</code>.
    */
   public static final String MIME_TYPE_APPLICATION_X_SHOCKWAVE_FLASH = "application/x-shockwave-flash";
+
+  /**
+   * Minimum Flash version ({@value}), which supports <code>ExternalInterface</code>.
+   */
   private static final int EXTERNAL_INTERFACE_MINIMUM_MAJOR_VERSION = 8;
 
   private static FlashMovieImpl impl;
