0bc2c31f8ee2f2d6d64b79494f9b708f305da8e6
Fred Sauer <fredsa@google.com>
2011-03-10 18:24:22 +0000

    Updated test code
    git-svn-id: https://gwt-voices.googlecode.com/svn/trunk@375 7bc97cf3-a739-0410-90d3-b551d870f104

--- Voices/test/com/allen_sauer/gwt/voices/test/client/VoicesTest.java
+++ Voices/test/com/allen_sauer/gwt/voices/test/client/VoicesTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2009 Fred Sauer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -19,12 +19,18 @@ import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.DataResource;
+import com.google.gwt.resources.client.DataResource.DoNotEmbed;
+import com.google.gwt.resources.client.DataResource.MimeType;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import com.allen_sauer.gwt.voices.client.FlashSound;
+import com.allen_sauer.gwt.voices.client.Html5Sound;
 import com.allen_sauer.gwt.voices.client.Sound;
 import com.allen_sauer.gwt.voices.client.SoundController;
 import com.allen_sauer.gwt.voices.client.handler.PlaybackCompleteEvent;
@@ -33,9 +39,25 @@ import com.allen_sauer.gwt.voices.client.handler.SoundLoadStateChangeEvent;
 
 // CHECKSTYLE_JAVADOC_OFF
 public class VoicesTest implements EntryPoint {
+  interface Bundle extends ClientBundle {
+    Bundle RESOURCES = GWT.create(Bundle.class);
+
+    @MimeType("audio/mpeg")
+    @Source("28917__junggle__btn107.mp3")
+    DataResource junggle();
+
+    @MimeType("audio/mpeg")
+    @Source("28917__junggle__btn107.mp3")
+    @DoNotEmbed
+    DataResource junggleNoEmbed();
+
+    @Source("36846__EcoDTR__LaserRocket.mp3")
+    DataResource laserRocket();
+  }
+
   private static native String getCompatMode()
   /*-{
-    return $doc.compatMode;
+		return $doc.compatMode;
   }-*/;
 
   /**
@@ -82,35 +104,66 @@ public class VoicesTest implements EntryPoint {
 
     addTest();
   }
+  private void addButton(SoundController sc, String mimeType, String url) {
+    final Sound sound = sc.createSound(mimeType, url, false);
+    sound.addEventHandler(new SoundHandler() {
+      public void onPlaybackComplete(PlaybackCompleteEvent event) {
+        System.out.println(event);
+      }
+
+      public void onSoundLoadStateChange(SoundLoadStateChangeEvent event) {
+        System.out.println(event);
+      }
+    });
+    Button button = new Button(url);
+    button.addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        sound.play();
+      }
+    });
+    RootPanel.get().add(button);
+  }
+
+  private void addButton(final String[] urls, String[] mimeTypes, SoundController sc, int i) {
+    final Sound sound = sc.createSound(mimeTypes[i], urls[i], false);
+    sound.addEventHandler(new SoundHandler() {
+      public void onPlaybackComplete(PlaybackCompleteEvent event) {
+        System.out.println(event);
+      }
+
+      public void onSoundLoadStateChange(SoundLoadStateChangeEvent event) {
+        System.out.println(event);
+      }
+    });
+    Button button = new Button(urls[i]);
+    final int index = i;
+    button.addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        sound.play();
+      }
+    });
+    RootPanel.get().add(button);
+  }
 
   @SuppressWarnings("deprecation")
   private void addTest() {
-    String[] urls = new String[] {
+    final String[] urls = new String[] {
         "freesoundproject/36846__EcoDTR__LaserRocket.mp3",
         "freesoundproject/22740__FranciscoPadilla__37_Click_Finger.wav",
         "http://media3.7digital.com/clips/34/2934485.clip.mp3",};
     String[] mimeTypes = new String[] {
         Sound.MIME_TYPE_AUDIO_MPEG, Sound.MIME_TYPE_AUDIO_X_WAV, Sound.MIME_TYPE_AUDIO_MPEG,};
-    SoundController soundController = new SoundController();
+    SoundController sc = new SoundController();
+    sc.setPreferredSoundType(FlashSound.class);
+    sc.setPreferredSoundType(Html5Sound.class);
 
     for (int i = 0; i < urls.length; i++) {
-      final Sound sound = soundController.createSound(mimeTypes[i], urls[i], true);
-      sound.addEventHandler(new SoundHandler() {
-        public void onPlaybackComplete(PlaybackCompleteEvent event) {
-          System.out.println(event);
-        }
-
-        public void onSoundLoadStateChange(SoundLoadStateChangeEvent event) {
-          System.out.println(event);
-        }
-      });
-      Button button = new Button(urls[i]);
-      button.addClickHandler(new ClickHandler() {
-        public void onClick(ClickEvent event) {
-          sound.play();
-        }
-      });
-      RootPanel.get().add(button);
+      String mimeType = mimeTypes[i];
+      String url = urls[i];
+      addButton(sc, mimeType, url);
     }
+    addButton(sc, Sound.MIME_TYPE_AUDIO_MPEG, Bundle.RESOURCES.laserRocket().getUrl());
+    addButton(sc, Sound.MIME_TYPE_AUDIO_MPEG, Bundle.RESOURCES.junggle().getUrl());
+    addButton(sc, Sound.MIME_TYPE_AUDIO_MPEG, Bundle.RESOURCES.junggleNoEmbed().getUrl());
   }
 }
